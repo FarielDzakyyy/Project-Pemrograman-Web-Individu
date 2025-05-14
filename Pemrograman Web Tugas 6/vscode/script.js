7308584e197fb743e8eb5f97446f41b0b6d03e8d
Fariel Dzaky <169522892+FarielDzakyyy@users.noreply.github.com>
2025-05-14 12:51:01 +0700

    Membuat Project Pemrograman Web Tugas 6

---
+++ Pemrograman Web Tugas 6/vscode/script.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+Nama = "Fariel Dzaky"
+console.log(Nama)
+
+Nim = 53
+console.log(Nim)
+
+Prodi = "Informatika"
+console.log(Prodi)
+
+Kelas = "B"
+console.log(Kelas)
+
+Umur = 20
+console.log(Umur)
+
+Agama = "Islam"
+console.log(Agama)
+
+JenisKelamin = "Pria"
+console.log(JenisKelamin)
+
+TinggiBadan = 178
+console.log(TinggiBadan)
+
+BeratBadan = 76
+console.log(BeratBadan)
