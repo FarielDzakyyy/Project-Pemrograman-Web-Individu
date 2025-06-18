5b8cb18935086f8ae58523ada560f1bfa1fd94b3
Fariel Dzaky <169522892+FarielDzakyyy@users.noreply.github.com>
2025-06-18 14:01:02 +0700

    Membuat Project Pemrograman Web Tugas 9

---
+++ Pemrograman Web Tugas 9/datamahasiswa.php
@@ -0,0 +1,49 @@
+<?php
+    
+    require 'function.php';
+    $query = "SELECT * FROM mahasiswa";
+    $rows = query($query); ///hasilnya wadah dengan isinya
+
+    
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA MAHASISWA</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+
+    <a href="tambahdata.php" ><button style="margin-bottom: 12px; background-color: lightgrey; ">Tambah Data</button></a>
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <th>No</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+            <th>No. HP</th>
+        </tr>
+        <?php 
+        $i = 1;
+        foreach ($rows as $mhs) { ?>
+        <tr>
+           <td><?= $i ?></td>
+           <td><img src="IMAGES/<?= $mhs['foto']; ?>" alt="<?= $mhs['nama'];?>"width="125"></td>
+           <td><?= $mhs["nama"] ?></td>
+           <td><?= $mhs["nim"] ?></td>
+           <td><?= $mhs["jurusan"] ?></td>
+           <td><?= $mhs["nohp"] ?></td>
+           <td><a href="hapusdata.php/?id=<?= $mhs["id"] ?>" ><button style="margin-bottom: 12px; background-color: red; ">Hapus</button></a></td>
+        </tr>
+        <?php $i++; } ?>
+
+    </table>
+</body>
+</html>
---
+++ Pemrograman Web Tugas 9/function.php
@@ -0,0 +1,52 @@
+<?php
+    $koneksi = mysqli_connect("localhost:3306", "root", "", "webif");
+
+    if(!$koneksi)
+    {
+        die("koneksi Gagal".mysqli_connect_error());
+    }
+
+    function query($query)
+    {
+      global $koneksi;
+       $result = mysqli_query($koneksi, $query);
+       
+       $rows = [];
+
+       while ($row = mysqli_fetch_assoc($result) )
+       {
+         $rows[] = $row;
+       }
+
+       return $rows;
+    }
+
+
+    function tambahmahasiswa($data)
+    {
+        global $koneksi;
+
+        $nama = $data["nama"];
+        $nim = $data["nim"];
+        $jurusan = $data["jurusan"];
+        $nohp = $data["nohp"];
+
+        $query = "INSERT INTO mahasiswa VALUES ('','','$nama','$nim','$jurusan','$nohp')";
+        
+        mysqli_query($koneksi, $query);
+
+        return mysqli_affected_rows($koneksi);
+
+    }
+
+    function hapusdata($id)
+    {
+        global $koneksi;
+        $query = "DELETE FROM mahasiswa where id=$id";
+        mysqli_query($koneksi,$query);
+
+        return mysqli_affected_rows($koneksi);
+    }
+    
+
+?>
